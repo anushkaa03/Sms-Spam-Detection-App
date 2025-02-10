743700b67116d7b325827c74446ee315704d6685
Anushka Ojha <146817398+anushkaa03@users.noreply.github.com>
2025-02-10 15:00:45 +0530

    Update sms-spamApp.py

--- sms-spamApp.py
+++ sms-spamApp.py
@@ -1,22 +1,61 @@
+import nltk
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
 import streamlit as st
-import pickle
+import pickle 
+import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
-model = pickle.load(open('spam.pkl','rb'))
-cv = pickle.load(open('vectorizer.pkl','rb'))
+ps = PorterStemmer()
 
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+
+tk = pickle.load(open("vectorizer.pkl", 'rb'))
+model = pickle.load(open("model.pkl", 'rb'))
 
 st.title("📱 SMS SPAM DETECTION MODEL")
 st.write("Made by Anushka Ojha 🚀")
+    
 
+input_sms = st.text_input("Enter the SMS")
 
-if st.button("Classify") :
-    if user_input:
-        data = [user_input]
-        vectorized_data = cv.transform(data).toarray()
-        result = model.predict(vectorized_data)
-        if result[0]==0:
-            st.write("The email is not spam")
-        else:
-            st.write("The email is spam")
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tk.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
     else:
-        st.write("Please type Email to classify")
+        st.header("Not Spam")
